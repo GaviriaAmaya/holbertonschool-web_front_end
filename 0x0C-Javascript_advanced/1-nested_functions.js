7cb562e20643d424dd91d50932d91470a1f18d2f
GaviriaAmaya <729@holbertonschool.com>
2020-09-21 18:41:09 -0500

    ¨[Add] Javascript Closure Scope Chain

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,21 @@
+// Closure scope chain
+let globalVariable = "Welcome ";
+
+function outer() {
+	alert(globalVariable);
+
+	let course = "Holberton";
+
+	function inner() {
+		alert(globalVariable + course);
+
+		let exclamation = '!';
+
+		function inception() {
+			alert(globalVariable + course + exclamation);
+		}
+		inception();
+	}
+	inner();
+}
+outer();
