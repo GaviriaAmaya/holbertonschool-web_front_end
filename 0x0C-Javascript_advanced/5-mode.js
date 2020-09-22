b19f2b0d866d2cedce3ca9428792d8062fa32fb6
GaviriaAmaya <729@holbertonschool.com>
2020-09-22 01:02:40 -0500

    [Add] Changes the color mode of the DOM

---
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -0,0 +1,51 @@
+// Function to change the DOM color mode
+/**
+ * Change the styles of the DOM
+ * @param {Number} size - Font size property
+ * @param {String} weight - Font weight property
+ * @param {String} transform - Text transform property
+ * @param {String} background - Background color property
+ * @param {String} color - Text color property
+ * @returns {function} styling - Closure to allow the calling in a local scope
+ */
+function changeMode(size, weight, transform, background, color) {
+	return styling => {
+		document.body.style.fontSize = size;
+		document.body.style.fontWeight = weight;
+		document.body.style.textTransform = transform;
+		document.body.style.backgroundColor = background;
+		document.body.style.color = color;
+	}
+}
+
+/**
+ * Changes the dom based on different parameters
+ */
+function main() {
+	let spooky = changeMode(9, "bold", "uppercase", "pink", "green");
+	let darkMode = changeMode(12, "bold", "capitalize", "black", "white");
+	let screamMode = changeMode(12, "normal", "lowercase", "white", "black");
+
+	/**
+	 * Creates an element and appends to the dom. If element is a button, creates event on click
+	 * according to the third parameter
+	 * @param {string} element - HTML element to be created
+	 * @param {string} innerHTML - Text to add to the HTML element created
+	 * @param {function} changeFunction - [Optional] Function at click event
+	 */
+	function createSpecificElement(element, innerHTML, changeFunction) {
+		let newElement = document.createElement(element);
+		newElement.innerHTML = innerHTML;
+		if (element == 'button') {
+			newElement.addEventListener("click", function() {
+				changeFunction();
+		})};
+		document.body.appendChild(newElement);
+	}
+
+	let paragraph = createSpecificElement('p', "Welcome Holberton!");
+	let spkyButton = createSpecificElement('button', "Spooky", spooky);
+	let darkButton = createSpecificElement('button', "Dark mode", darkMode);
+	let scrmButton = createSpecificElement('button', "Scream mode", screamMode);
+}
+main();
